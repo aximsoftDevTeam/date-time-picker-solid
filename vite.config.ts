60fd6c1677c32e3192bebf7b3e71ffaa8d89a2c3
aximsiva <sivashankaran.k@aximsoft.com>
2023-06-08 12:26:16 +0530

    bundle up process

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "ViteButton",
+      fileName: (format) => `index.${format}.js`,
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [solidPlugin(), dts()],
   server: {
     port: 3000,
-  },
-  build: {
-    target: 'esnext',
   },
 });
